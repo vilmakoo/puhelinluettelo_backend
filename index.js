431bb0714f00b24642c83a44aedb0c9e225ea301
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-02-02 14:48:50 +0200

    Deploy frontend

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(morgan(':method :url :data :status :res[content-length] - :response-time
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 let persons = [
     {
         id: 1,
@@ -91,5 +93,3 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-console.log('äää')
